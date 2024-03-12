d5ea8fd380de89aebc477195f5f82800693037a2
miakojason <miakojason@gmail.com>
2024-03-12 11:46:44 +0800

    res

--- api/save_poster.php
+++ api/save_poster.php
@@ -18,7 +18,7 @@ if (isset($_POST['id'])) {
         move_uploaded_file($_FILES['poster']['tmp_name'], "../img/" . $_FILES['poster']['name']);
         $_POST['img'] = $_FILES['poster']['name'];
     }
-    $_POST['rank'] = $Poster->max('id');
+    $_POST['rank'] = $Poster->max('id')+1;
     $_POST['ani'] = rand(1, 3);
     $Poster->save($_POST);
 }
